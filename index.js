ee0a28eb64dbf26a13642fd481121649e809fbdf
Jannis R <mail@jannisr.de>
2018-08-18 15:43:57 +0200

    optionally use hafas-client-rpc

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const createHafas = require('hafas-client')
 const bvgProfile = require('hafas-client/p/bvg')
 const createApi = require('hafas-rest-api')
 
@@ -17,14 +16,35 @@ const config = {
 	aboutPage: true
 }
 
-const hafas = createHafas(bvgProfile, 'bvg-rest')
-const api = createApi(hafas, config, () => {})
+const onError = (err) => {
+	console.error(err)
+	process.exitCode = 1
+}
 
-api.listen(config.port, (err) => {
-	if (err) {
-		console.error(err)
-		process.exitCode = 1
-	} else {
-		console.info(`Listening on ${config.hostname}:${config.port}.`)
-	}
-})
+const startApi = (hafas) => {
+	const api = createApi(hafas, config, () => {})
+
+	api.listen(config.port, (err) => {
+		if (err) onError(err)
+		else console.info(`Listening on ${config.hostname}:${config.port}.`)
+	})
+}
+
+if (process.env.HAFAS_CLIENT_NODES) {
+	const createRoundRobin = require('@derhuerst/round-robin-scheduler')
+	const createRpcClient = require('hafas-client-rpc/client')
+
+	const nodes = process.env.HAFAS_CLIENT_NODES.split(',')
+	console.info('Using these hafas-client-rpc nodes:', nodes)
+
+	createRpcClient(createRoundRobin, nodes, (err, rpcHafas) => {
+		if (err) return onError(err)
+		rpcHafas.profile = bvgProfile
+		startApi(rpcHafas)
+	})
+} else {
+	const createHafas = require('hafas-client')
+
+	const hafas = createHafas(bvgProfile, 'bvg-rest')
+	startApi(hafas)
+}
