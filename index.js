0d911cf2aca78029290922099b2fe5b9f3a1ad5c
Jannis R <mail@jannisr.de>
2018-11-20 23:51:38 +0100

    refactor index.js

--- index.js
+++ index.js
@@ -5,6 +5,28 @@ const createApi = require('hafas-rest-api')
 
 const pkg = require('./package.json')
 
+const pHafas = (() => {
+	if (!process.env.HAFAS_CLIENT_NODES) {
+		const createHafas = require('hafas-client')
+		const hafas = createHafas(bvgProfile, 'bvg-rest')
+		return Promise.resolve(hafas)
+	}
+
+	const createRoundRobin = require('@derhuerst/round-robin-scheduler')
+	const createRpcClient = require('hafas-client-rpc/client')
+
+	const nodes = process.env.HAFAS_CLIENT_NODES.split(',')
+	console.info('Using these hafas-client-rpc nodes:', nodes)
+
+	return new Promise((resolve, reject) => {
+		createRpcClient(createRoundRobin, nodes, (err, rpcHafas) => {
+			if (err) return reject(err)
+			rpcHafas.profile = bvgProfile
+			resolve(rpcHafas)
+		})
+	})
+})()
+
 const config = {
 	hostname: process.env.HOSTNAME || '1.bvg.transport.rest',
 	port: process.env.PORT || 3000,
@@ -21,30 +43,13 @@ const onError = (err) => {
 	process.exitCode = 1
 }
 
-const startApi = (hafas) => {
+pHafas
+.then((hafas) => {
 	const api = createApi(hafas, config, () => {})
 
 	api.listen(config.port, (err) => {
 		if (err) onError(err)
 		else console.info(`Listening on ${config.hostname}:${config.port}.`)
 	})
-}
-
-if (process.env.HAFAS_CLIENT_NODES) {
-	const createRoundRobin = require('@derhuerst/round-robin-scheduler')
-	const createRpcClient = require('hafas-client-rpc/client')
-
-	const nodes = process.env.HAFAS_CLIENT_NODES.split(',')
-	console.info('Using these hafas-client-rpc nodes:', nodes)
-
-	createRpcClient(createRoundRobin, nodes, (err, rpcHafas) => {
-		if (err) return onError(err)
-		rpcHafas.profile = bvgProfile
-		startApi(rpcHafas)
-	})
-} else {
-	const createHafas = require('hafas-client')
-
-	const hafas = createHafas(bvgProfile, 'bvg-rest')
-	startApi(hafas)
-}
+})
+.catch(onError)
