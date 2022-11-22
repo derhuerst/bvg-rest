686f6b570dfa39043978d5d5e8c3bd44a8955c98
Jannis R <mail@jannisr.de>
2022-11-22 17:41:07 +0100

    add test for /stations ✅
    bvg-rest equivalent of derhuerst/db-rest@f982e15 & derhuerst/vbb-rest@0d5db65

--- test/index.js
+++ test/index.js
@@ -2,6 +2,24 @@
 
 const tape = require('tape')
 const {fetchWithTestApi} = require('./util')
+const {allStations} = require('../lib/vbb-stations')
+
+tape.test('/stops works', async (t) => {
+	const someStationId = allStations[0].id
+	console.error({someStationId})
+
+	{
+		const {headers, data} = await fetchWithTestApi({}, '/stops', {
+			headers: {
+				'accept': 'application/json',
+			},
+		})
+		t.equal(headers['content-type'], 'application/json; charset=utf-8')
+		t.ok(Array.isArray(data))
+		t.ok(data.find(({id}) => id === someStationId))
+		t.equal(data.length, Object.keys(allStations).length)
+	}
+})
 
 tape.test('/stops?query=hauptbah works', async (t) => {
 	const BERLIN_HBF = '900000003201'
