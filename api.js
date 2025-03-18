04a6b28e31fbc519f747ea093bd2d3b5b5b5b229
Traines <git@traines.eu>
2025-03-18 14:22:41 +0100

    fix Content-Security-Policy header 🐛
    cherry-picked from https://github.com/derhuerst/db-rest/commit/ac7d86f962ed24f5b30013c436c3bad538cb9e81
    
    Co-Authored-By: Jannis R <mail@jannisr.de>

--- api.js
+++ api.js
@@ -95,7 +95,7 @@ const config = {
 	logging: true,
 	aboutPage: false,
 	etags: 'strong',
-	csp: `default-src 'none' style-src 'self' 'unsafe-inline' img-src https:`,
+	csp: `default-src 'none'; style-src 'self' 'unsafe-inline'; img-src https:`,
 	healthCheck,
 	modifyRoutes,
 }
